e9e54a196871611d518affd52ed91c28aea875b2
giadasql <67785397+giadasql@users.noreply.github.com>
2020-12-21 21:46:15 +0100

    Aggiunta inizializzazione di PersistenceImplementation con lettura settings da XML

--- src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/persistence/Persistence.java
+++ src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/persistence/Persistence.java
@@ -1,4 +1,5 @@
 package it.unipi.iit.inginf.lsmdb.communitunes.persistence;
 
 public interface Persistence {
+
 }
--- src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/persistence/PersistenceImplementation.java
+++ src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/persistence/PersistenceImplementation.java
@@ -1,4 +1,49 @@
 package it.unipi.iit.inginf.lsmdb.communitunes.persistence;
 
+import it.unipi.iit.inginf.lsmdb.communitunes.utilities.configurations.ConfigReader;
+import it.unipi.iit.inginf.lsmdb.communitunes.utilities.configurations.ConfigReaderFactory;
+import it.unipi.iit.inginf.lsmdb.communitunes.utilities.configurations.ConfigReaderType;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+
 class PersistenceImplementation {
+
+    private ConfigReader reader;
+
+    private MongoDriver mongo;
+    private Neo4jDriver neo4j;
+
+    public PersistenceImplementation() {
+        String settingsFileName = "Settings.xml";
+        InputStream settingsFileStream = this.getClass().getClassLoader().getResourceAsStream(settingsFileName);
+        try{
+            reader = ConfigReaderFactory.CreateConfigReader(ConfigReaderType.Xml, settingsFileStream);
+        }
+        catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+            return;
+        }
+        if(reader == null){
+            // TODO: log
+            return;
+        }
+
+        neo4jInit();
+        mongoInit();
+    }
+
+    private void neo4jInit(){
+        String neo4jUri = reader.getStringConfigValue("Neo4j", "uri");
+        String neo4jUser = reader.getStringConfigValue("Neo4j", "username");
+        String neo4jPsw = reader.getStringConfigValue("Neo4j", "password");
+        neo4j = new Neo4jDriver(neo4jUri, neo4jUser, neo4jPsw);
+    }
+
+    private void mongoInit(){
+        String connectionString = reader.getStringConfigValue("MongoDB", "connectionString");
+        mongo = new MongoDriver(connectionString);
+    }
 }
