3aacecb39ebca56d8dc08f1574457a5bf34f04c2
Giada Beccari <giada.beccari@joob.srl>
2021-02-04 23:14:40 +0100

    User Profile page

---
+++ src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/frontend/controllers/UserProfileController.java
@@ -0,0 +1,15 @@
+package it.unipi.iit.inginf.lsmdb.communitunes.frontend.controllers;
+
+import it.unipi.iit.inginf.lsmdb.communitunes.frontend.LayoutManager;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class UserProfileController implements UIController {
+    @FXML
+    public Text username;
+
+    @Override
+    public void init(LayoutManager manager) {
+
+    }
+}
