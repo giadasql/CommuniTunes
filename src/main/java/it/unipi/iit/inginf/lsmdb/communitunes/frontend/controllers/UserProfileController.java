2228ec1baddc8b60fc4c668b3c70bc03b7c59cf2
Giada Beccari <giada.beccari@joob.srl>
2021-02-06 00:15:08 +0100

    User profile with user info in VBox

--- src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/frontend/controllers/UserProfileController.java
+++ src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/frontend/controllers/UserProfileController.java
@@ -4,7 +4,13 @@ import it.unipi.iit.inginf.lsmdb.communitunes.entities.User;
 import it.unipi.iit.inginf.lsmdb.communitunes.frontend.context.LayoutManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
@@ -16,19 +22,76 @@ public class UserProfileController implements UIController {
     public Button followUnfollow;
     public Circle avatarCircle;
     public Button editProfile;
+    public VBox infoVBox;
+    public Text country;
+    public Text birthday;
+    public Text lastName;
+    public Text firstName;
+    public HBox lastNameBox;
+    public HBox firstNameBox;
+    public HBox countryBox;
+    public HBox birthdayBox;
 
     private User user;
 
     @Override
     public void init(LayoutManager manager) {
         user = manager.context.focusedUser;
+        username.setText(user.Username);
+        if(user.Avatar != null){
+            Image avatar = new Image(user.Avatar);
+            avatarCircle.setFill(new ImagePattern(avatar));
+            avatarCircle.setEffect(new DropShadow(+25d, 0d, +2d, Color.BLUEVIOLET));
+        }
+        // check if the profile belongs to the user that is currently logged in
         if(manager.context.authenticatedUser.Username.equals(user.Username)){
             editProfile.setVisible(true);
+            editProfile.setManaged(true);
             followUnfollow.setVisible(false);
+            followUnfollow.setManaged(false);
         }
         else{
             editProfile.setVisible(false);
+            editProfile.setManaged(false);
             followUnfollow.setVisible(true);
+            followUnfollow.setManaged(true);
+        }
+        // write info in the infoContainer
+        if(user.FirstName != null){
+            firstName.setText(user.FirstName);
+            firstNameBox.setVisible(true);
+            firstNameBox.setManaged(true);
+        }
+        else {
+            firstNameBox.setVisible(false);
+            firstNameBox.setManaged(false);
+        }
+        if(user.LastName != null){
+            lastName.setText(user.LastName);
+            lastNameBox.setVisible(true);
+            lastNameBox.setManaged(true);
+        }
+        else{
+            lastNameBox.setVisible(false);
+            lastNameBox.setManaged(false);
+        }
+        if(user.Country != null){
+            country.setText(user.Country);
+            countryBox.setVisible(true);
+            countryBox.setManaged(true);
+        }
+        else{
+            countryBox.setVisible(false);
+            countryBox.setManaged(false);
+        }
+        if(user.Birthday != null){
+            birthday.setText(user.Birthday.toString());
+            birthdayBox.setVisible(true);
+            birthdayBox.setManaged(true);
+        }
+        else{
+            birthdayBox.setVisible(false);
+            birthdayBox.setManaged(false);
         }
     }
 }
