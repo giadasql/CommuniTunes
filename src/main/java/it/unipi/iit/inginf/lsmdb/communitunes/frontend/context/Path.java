0c768eb739eadfa4298f5ea0acbe956cba0de7cb
FranDT <73546916+FranDT@users.noreply.github.com>
2021-02-13 02:05:42 +0100

    Bug fixes

--- src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/frontend/context/Path.java
+++ src/main/java/it/unipi/iit/inginf/lsmdb/communitunes/frontend/context/Path.java
@@ -23,4 +23,5 @@ public class Path {
     public static final String FOLLOWED_ARTISTS = "ui/pages/followed_artists.fxml";
     public static final String FOLLOWER_ARTISTS = "ui/pages/follower_artists.fxml";
     public static final String REVIEWS = "ui/pages/song/reviews_page.fxml";
+    public static final String ARTIST_REQUEST_PAGE = "ui/pages/user/artist_request_page.fxml";
 }
